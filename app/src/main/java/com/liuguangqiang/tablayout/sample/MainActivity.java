15f38e6aacae3127a0bc7be5917eafdd99dce48a
Eric <guangqiang.dev@gmail.com>
2015-05-06 14:14:25 +0800

    updated sample and version.

--- app/src/main/java/com/liuguangqiang/tablayout/sample/MainActivity.java
+++ app/src/main/java/com/liuguangqiang/tablayout/sample/MainActivity.java
@@ -2,8 +2,14 @@ package com.liuguangqiang.tablayout.sample;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toast;
 
+import com.liuguangqiang.multilayout.widget.Page;
 import com.liuguangqiang.multilayout.widget.MultiLayout;
+import com.liuguangqiang.multilayout.widget.OnPageChangedListener;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -23,18 +29,47 @@ public class MainActivity extends ActionBarActivity {
     }
 
     private void initViews() {
-        multiLayout.addLayout(R.layout.layout_tab1);
-        multiLayout.addLayout(R.layout.layout_tab2);
+        multiLayout.addPage(R.layout.layout_tab1);
+        multiLayout.addPage(R.layout.layout_tab2);
+
+        Page layoutItem = new Page(this);
+        View view = LayoutInflater.from(this).inflate(R.layout.layout_tab3, null);
+        layoutItem.setContent(view);
+        multiLayout.addPage(layoutItem);
+        multiLayout.setOnPageChangedListener(new OnPageChangedListener() {
+            @Override
+            public void onOpen(int position) {
+                Log.i("MultiLayout", "onOpen:" + position);
+//                Toast.makeText(getApplicationContext(), "open:" + position, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onClose(int position) {
+                Log.i("MultiLayout", "onClose:" + position);
+//                Toast.makeText(getApplicationContext(), "close:" + position, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onFinished(int position, boolean isOpen) {
+                Log.i("MultiLayout", String.format("onFinished:p-%s,b-%s", position, isOpen));
+//                Toast.makeText(getApplicationContext(), "onFinished:" + position, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @OnClick(R.id.btn_tab1)
     public void onClickTab1() {
-        multiLayout.showTab(0);
+        multiLayout.open(0);
     }
 
     @OnClick(R.id.btn_tab2)
     public void onClickTab2() {
-        multiLayout.showTab(1);
+        multiLayout.open(1);
+    }
+
+    @OnClick(R.id.btn_tab3)
+    public void onClickTab3() {
+        multiLayout.open(2);
     }
 
 }
