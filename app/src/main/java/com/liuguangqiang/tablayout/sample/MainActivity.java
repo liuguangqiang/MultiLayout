bf8f942701b529d10f8b2b9c51bc0a1046df3124
Eric <guangqiang.dev@gmail.com>
2015-05-08 09:33:48 +0800

    Update sample.

--- app/src/main/java/com/liuguangqiang/tablayout/sample/MainActivity.java
+++ app/src/main/java/com/liuguangqiang/tablayout/sample/MainActivity.java
@@ -5,11 +5,10 @@ import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Toast;
 
-import com.liuguangqiang.multilayout.widget.Page;
 import com.liuguangqiang.multilayout.widget.MultiLayout;
 import com.liuguangqiang.multilayout.widget.OnPageChangedListener;
+import com.liuguangqiang.multilayout.widget.Page;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -33,6 +32,7 @@ public class MainActivity extends ActionBarActivity {
         multiLayout.addPage(R.layout.layout_tab2);
 
         Page layoutItem = new Page(this);
+        layoutItem.setGravity(Page.Gravity.TOP);
         View view = LayoutInflater.from(this).inflate(R.layout.layout_tab3, null);
         layoutItem.setContent(view);
         multiLayout.addPage(layoutItem);
@@ -40,19 +40,11 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public void onOpen(int position) {
                 Log.i("MultiLayout", "onOpen:" + position);
-//                Toast.makeText(getApplicationContext(), "open:" + position, Toast.LENGTH_LONG).show();
             }
 
             @Override
             public void onClose(int position) {
                 Log.i("MultiLayout", "onClose:" + position);
-//                Toast.makeText(getApplicationContext(), "close:" + position, Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onFinished(int position, boolean isOpen) {
-                Log.i("MultiLayout", String.format("onFinished:p-%s,b-%s", position, isOpen));
-//                Toast.makeText(getApplicationContext(), "onFinished:" + position, Toast.LENGTH_LONG).show();
             }
         });
     }
